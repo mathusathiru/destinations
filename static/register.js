08cca97cf849a4a694678d7f62fe5247f0cb67ad
mathusathiru <mathu_thiru@icloud.com>
2023-08-14 09:40:59 +0100

    Update JavaScript

--- static/register.js
+++ static/register.js
@@ -15,34 +15,40 @@ document.addEventListener("DOMContentLoaded", function() {
     const password = passwordInput.value;
 
     let errorMessages = [];
+
     if (username.length < 3) {
-      errorMessages.push("Username must be at least three characters");
+      errorMessages.push("Username (min. 3 characters)");
     }
+
     if (password.length < 8) {
-      errorMessages.push("Password must be at least eight characters");
+      errorMessages.push("Password (min. 8 characters)");
     }
 
-    // Clear previous error messages
-    while (usernameError.firstChild) {
-      usernameError.removeChild(usernameError.firstChild);
-    }
-    while (passwordError.firstChild) {
-      passwordError.removeChild(passwordError.firstChild);
-    }
+    for (const message of errorMessages) {
+      const errorDiv = document.createElement("div");
+      errorDiv.classList.add("error-message");
+      errorDiv.textContent = message;
 
-    if (errorMessages.length > 0) {
-      for (const message of errorMessages) {
-        const errorDiv = document.createElement("div");
-        errorDiv.classList.add("error-message");
-        errorDiv.textContent = message;
-
-        if (message.includes("Username")) {
-          usernameError.appendChild(errorDiv);
-        } else if (message.includes("Password")) {
-          passwordError.appendChild(errorDiv);
-        }
+      if (message.includes("Username")) {
+        usernameError.appendChild(errorDiv);
+      } else if (message.includes("Password")) {
+        passwordError.appendChild(errorDiv);
       }
-    } else {
+    }
+
+    const noErrorDiv = document.createElement("div");
+    noErrorDiv.classList.add("no-error");
+    noErrorDiv.textContent = "No error";
+
+    if (username.length >= 3) {
+      usernameError.appendChild(noErrorDiv);
+    }
+
+    if (password.length >= 8) {
+      passwordError.appendChild(noErrorDiv);
+    }
+
+    if (errorMessages.length === 0) {
       registerForm.submit();
     }
   });
--- static/search.js
+++ static/search.js
@@ -1,33 +1,64 @@
-document.getElementById('searchForm').addEventListener('submit', function (event) {
-    event.preventDefault(); 
+document.getElementById("search-form").addEventListener('submit', async function (event) {
+    event.preventDefault();
     const form = event.target;
     const formData = new FormData(form);
 
-    fetch(form.action, {
-        method: 'POST',
-        body: formData,
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            document.getElementById('hiddenParagraph').innerText = data.error;
-        } else {
-            if (data.results) {
-                const resultsContainer = document.getElementById('resultsContainer');
-                resultsContainer.innerHTML = '';
+    // Collect the selected radius value
+    const selectedRadius = document.querySelector('#radius-buttons input[name="radius"]:checked');;
+    if (selectedRadius) {
+        formData.append('radius', selectedRadius.value);
 
-                const resultsList = document.createElement('ul');
+        // Clear the error message
+        document.getElementById('error-message').innerText = '';
+
+        // Remove the previous map if it exists
+        const mapContainer = document.getElementById('map');
+        if (mapContainer) {
+            mapContainer.remove();
+        }
+
+        // Remove the previous results list
+        const resultsContainer = document.getElementById('results-container');
+        resultsContainer.innerHTML = '';
+
+        try {
+            const response = await fetch(form.action, {
+                method: 'POST',
+                body: formData,
+            });
+            const data = await response.json();
+
+            if (data.error) {
+                document.getElementById('error-message').innerText = data.error;
+            } else if (data.results) {
+                const resultsContainer = document.getElementById('results-container');
+
+                // Create the Leaflet map
+                const newMapContainer = document.createElement('div');
+                newMapContainer.id = 'map';
+                newMapContainer.style.width = '100%';
+                newMapContainer.style.height = '500px';
+                resultsContainer.appendChild(newMapContainer);
+
+                // Initialize the map
+                const map = L.map('map').setView([51.61794, -0.017785], 14); // Set initial map view
+
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
+
+                // Add markers to the map
                 data.results.forEach(result => {
-                    const listItem = document.createElement('li');
-                    listItem.textContent = `Place Name: ${result.name}\nAddress: ${result.location.formatted_address}`;
-                    resultsList.appendChild(listItem);
+                    const marker = L.marker([result.geocodes.main.latitude, result.geocodes.main.longitude]).addTo(map);
+                    marker.bindPopup(`<b>${result.name}</b><br>${result.location.formatted_address}`);
                 });
 
-                resultsContainer.appendChild(resultsList);
             }
+        } catch (error) {
+            document.getElementById('error-message').innerText = 'An error occurred during the search.';
         }
-    })
-    .catch(error => {
-        document.getElementById('hiddenParagraph').innerText = 'An error occurred during the search.';
-    });
+    } else {
+        // Display error message if no radius is selected
+        document.getElementById('error-message').innerText = 'Please select a radius.';
+    }
 });
