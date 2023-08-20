678a4a381795492084eacc6d817e23b0c4b57fc0
mathusathiru <mathu_thiru@icloud.com>
2023-08-20 13:01:19 +0100

    Update system tests to comply with style checks

--- system-tests.py
+++ system-tests.py
@@ -1,17 +1,17 @@
 from helium import *
 
 # Test homepage
-start_chrome("http://127.0.0.1:5000/", maximize = "True")
+start_chrome("http://127.0.0.1:5000/", maximize="True")
 wait_until(Text("elevate guest experiences").exists)
 kill_browser()
 
 # Test search page
-start_chrome("http://127.0.0.1:5000/search.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/search.html", maximize="True")
 wait_until(Text("Radius Selection").exists)
 kill_browser()
 
 # Test search page functionality
-start_chrome("http://127.0.0.1:5000/search.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/search.html", maximize="True")
 click(RadioButton("500"))
 click(S("#search"))
 write("SW1A 0AA", into = S("#search"))
@@ -20,7 +20,7 @@ wait_until(Text("OpenStreetMap").exists)
 kill_browser()
 
 # Test registration
-start_chrome("http://127.0.0.1:5000/register.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/register.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Register"))
@@ -28,7 +28,7 @@ wait_until(Text("Welcome back newuser!").exists)
 kill_browser()
 
 # Test login
-start_chrome("http://127.0.0.1:5000/login.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/login.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Login"))
@@ -36,7 +36,7 @@ wait_until(Text("Welcome back newuser!").exists)
 kill_browser()
 
 # Test search records - with no records
-start_chrome("http://127.0.0.1:5000/login.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/login.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Login"))
@@ -49,7 +49,7 @@ wait_until(Text("No matching records found.").exists)
 kill_browser()
 
 # Test all searches - with no records
-start_chrome("http://127.0.0.1:5000/login.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/login.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Login"))
@@ -58,8 +58,8 @@ click(Link("All Locations"))
 wait_until(Text("Search history is empty").exists)
 kill_browser()
 
-# Test popular locatins - with no records 
-start_chrome("http://127.0.0.1:5000/login.html", maximize = "True")
+# Test popular locatins - with no records
+start_chrome("http://127.0.0.1:5000/login.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Login"))
@@ -69,7 +69,7 @@ wait_until(Text("Search history is empty").exists)
 kill_browser()
 
 # Test search page as a logged in user
-start_chrome("http://127.0.0.1:5000/login.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/login.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Login"))
@@ -83,7 +83,7 @@ wait_until(Text("OpenStreetMap").exists)
 kill_browser()
 
 # Test all searches - with records
-start_chrome("http://127.0.0.1:5000/login.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/login.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Login"))
@@ -93,7 +93,7 @@ wait_until(S(".search-result-card").exists)
 kill_browser()
 
 # Test popular locatins - with records
-start_chrome("http://127.0.0.1:5000/login.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/login.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Login"))
@@ -103,7 +103,7 @@ wait_until(S(".search-result-card").exists)
 kill_browser()
 
 # Test search past records  - with records
-start_chrome("http://127.0.0.1:5000/login.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/login.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Login"))
@@ -116,7 +116,7 @@ wait_until(S(".search-result-card").exists)
 kill_browser()
 
 # Test logout
-start_chrome("http://127.0.0.1:5000/login.html", maximize = "True")
+start_chrome("http://127.0.0.1:5000/login.html", maximize="True")
 write("newuser", into = "username")
 write("password123", into = "password")
 click(Button("Login"))
