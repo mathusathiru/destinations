290c3bcf37742082cedfa66203c5a5891f1ab510
mathusathiru <mathu_thiru@icloud.com>
2023-08-21 09:35:48 +0100

    Update JavaScript files with comments

--- static/register.js
+++ static/register.js
@@ -1,34 +1,47 @@
+// script executed when the HTML page is loaded
 document.addEventListener("DOMContentLoaded", function() {
+  // obtain form element with id "register-form"
   const registerForm = document.getElementById("register-form");
+  // obtain username and password input values through their respective IDs
   const usernameInput = document.getElementById("username");
   const passwordInput = document.getElementById("password");
+  // obtain error message placeholder elements for the username and password, through their respective IDs
   const usernameError = document.getElementById("username-error");
   const passwordError = document.getElementById("password-error");
 
+  // event listener for the form submit event (submit button), executes when submit event is activated
   registerForm.addEventListener("submit", function(event) {
+    // prevent default form submission behavior to execute validation checks
     event.preventDefault();
 
+    // clear previous error messages
     usernameError.textContent = "";
     passwordError.textContent = "";
 
+    // obtain values of username and password inputs, respectively
     const username = usernameInput.value;
     const password = passwordInput.value;
 
+    // initialise array to store error messages
     let errorMessages = [];
 
+    // check username length (valid usernames must be three characters or greater)
     if (username.length < 3) {
       errorMessages.push("Username too short (min. 3 characters)");
     }
 
+    // check password length (valid passwords must be eight characters or greater)
     if (password.length < 8) {
       errorMessages.push("Password too short (min. 8 characters)");
     }
 
+    // iterate through error messages in array, and display them through div elements
     for (const message of errorMessages) {
       const errorDiv = document.createElement("div");
       errorDiv.classList.add("error-message");
       errorDiv.textContent = message;
 
+      // append error message(s) to the correct div element(s)
       if (message.includes("Username")) {
         usernameError.appendChild(errorDiv);
       } else if (message.includes("Password")) {
@@ -36,18 +49,22 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
 
+    // create  div element to display a "No error" message (hidden on the page)
     const noErrorDiv = document.createElement("div");
     noErrorDiv.classList.add("no-error");
     noErrorDiv.textContent = "No error";
 
+    // append "No error" message to the username if it is three characters or greater
     if (username.length >= 3) {
       usernameError.appendChild(noErrorDiv);
     }
 
+    // append "No error" message to the password if it is eight characters or greater
     if (password.length >= 8) {
       passwordError.appendChild(noErrorDiv);
     }
 
+    // submit the form if there are no errors (identied by an empty array of error messages)
     if (errorMessages.length === 0) {
       registerForm.submit();
     }
--- static/search.js
+++ static/search.js
@@ -1,47 +1,65 @@
+// script executed on the submit event of search.html pager form (id #search-form)
 document.getElementById("search-form").addEventListener('submit', async function (event) {
+    // prevent default form submission behavior
     event.preventDefault();
+    // obtain form element triggering the event
     const form = event.target;
+    // create FormData object from the form
     const formData = new FormData(form);
 
-    const selectedRadius = document.querySelector('#radius-buttons input[name="radius"]:checked');;
+    // obtain selected radius from the radio buttons
+    const selectedRadius = document.querySelector('#radius-buttons input[name="radius"]:checked');
     if (selectedRadius) {
+        // append selected radius to FormData object
         formData.append('radius', selectedRadius.value);
 
+        // clear error message from a previous search
         document.getElementById('error-message').innerText = '';
 
+        // remove the map container if it already exists from a previous search
         const mapContainer = document.getElementById('map');
         if (mapContainer) {
             mapContainer.remove();
         }
 
+        // clear the results container if it already exists from a previous search
         const resultsContainer = document.getElementById('results-container');
         resultsContainer.innerHTML = '';
 
         try {
+            // send a POST request to the form action URL with the FormData
             const response = await fetch(form.action, {
-                method: 'POST',
+                method: "POST",
                 body: formData,
             });
+            // parse the response as JSON
             const data = await response.json();
 
+            // display an error message if an error occurs with the response data
             if (data.error) {
                 document.getElementById('error-message').innerText = data.error;
             } else if (data.results) {
+                // if results are present from the response data, display them on a map
                 const resultsContainer = document.getElementById('results-container');
 
+                // create a new map container element and append it to results container
                 const newMapContainer = document.createElement('div');
                 newMapContainer.id = 'map';
                 newMapContainer.style.width = '96.5%';
                 newMapContainer.style.height = '600px';
                 resultsContainer.appendChild(newMapContainer);
 
-                const map = L.map('map').setView([51.61794, -0.017785], 14);
+                // initialize a Leaflet map with a view centered on specific coordinates and zoom level
+                const map = L.map("map").setView([51.61794, -0.017785], 14);
 
+                // add a map tile layer using OpenStreetMap tiles and attribution
                 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(map);
 
+                // iterate through results, adding a marker for each result
                 data.results.forEach(result => {
+                    // create a custom marker icon using a png file
                     const customIcon = L.icon({
                         iconUrl: '/static/images/marker.png',
                         iconSize: [25, 41],
@@ -49,16 +67,18 @@ document.getElementById("search-form").addEventListener('submit', async function
                         popupAnchor: [1, -34],
                     });
                 
+                    // add marker to a coordinate utilising the custom icon
                     const marker = L.marker([result.geocodes.main.latitude, result.geocodes.main.longitude], { icon: customIcon }).addTo(map);
+                    // attach a popup to the marker displaying results information (place name and address)
                     marker.bindPopup(`<b>${result.name}</b><br>${result.location.formatted_address}`);
                 });
-                         
-
             }
         } catch (error) {
+            // display an error message if unknown errors occur during the search API calls
             document.getElementById('error-message').innerText = 'An error occurred during the search.';
         }
     } else {
+        // display an error message if no radius value is selected
         document.getElementById('error-message').innerText = 'Please select a radius.';
     }
 });
